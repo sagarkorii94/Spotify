365c5065dc8c27087aac168c695d79005b8a07a8
sagarkori <104709336+sagarkori@users.noreply.github.com>
2024-01-04 13:10:03 +0530

    first commit

--- component/MainContainer.js
+++ component/MainContainer.js
@@ -1,12 +1,55 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import "../Styles/MainContainer.css";
+import { FaUser } from 'react-icons/fa';
+import AudioList from "./AudioList";
+import  Banner from "./Banner";
 
 function MainContainer() {
+  useEffect(() => {
+    const allLi = document.querySelector(".menuList").querySelectorAll("li");
+
+    function changePopularActive() {
+      allLi.forEach((n) => n.classList.remove("active"));
+      this.classList.add("active");
+    }
+
+    allLi.forEach((n) => n.addEventListener("click", changePopularActive));
+  }, []);
+
   return (
-    <div className='mainContainer'>
-      
+    <div className="mainContainer">
+      <Banner />
+
+      <div className="menuList">
+        <ul>
+          <li>
+            <a href="#">Popular</a>
+          </li>
+          <li>
+            <a href="#">Albums</a>
+          </li>
+          <li>
+            <a href="#">Songs</a>
+          </li>
+          <li>
+            <a href="#">Fans</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+        </ul>
+
+        <p>
+          <i>
+            <FaUser />
+          </i>
+          12.3M <span>Followers</span>
+        </p>
+      </div>
+
+      <AudioList />
     </div>
-  )
+  );
 }
 
-export default MainContainer;
+export default MainContainer ;
